34d1055e6252f71547483e9ee3a84417b3cb9c35
ansil <ansil@Andrew-Laptop>
2019-03-10 18:31:01 -0500

    Menu añadido

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -2,6 +2,8 @@ package Main;
 
 import Loop1.LoopJuego;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
@@ -9,28 +11,22 @@ import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 public class Main extends Application{
-
 /**
  *
  * @author Estudiante
  */
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Pane layout  = new Pane();  
-        Canvas canvas = new Canvas(810,520);
-        layout.getChildren().add(canvas);
-        Scene escena = new Scene(layout,796,520);
-        
-        GraphicsContext lapiz = canvas.getGraphicsContext2D();
-        LoopJuego juego = new LoopJuego(escena, lapiz);
-        juego.start();
-        
-        primaryStage.setScene(escena);
-        primaryStage.setTitle("Ejemplo Escenario Interactivo");
-        primaryStage.show();
-        
+        //registrar la variable en el singleton
+        Singleton singleton = Singleton.getSingleton();
+        singleton.setStage(primaryStage);
+        //modeloen memoria
+        Modelo modelo = new Modelo();
+        //invocar el controlador de la vista que quiero visualizar
+        MenuController ventanaMenu = new MenuController(modelo);
+        ventanaMenu.mostrarVista();
     }
     public static void main(String[] args) {
-        Application.launch(args);
+        launch(args);
     }
 }
---
+++ src/Main/Modelo.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+/**
+ *
+ * @author gasilva
+ */
+public class Modelo {
+    private String x;
+    private String y;
+
+    
+
+    public String getX() {
+        return x;
+    }
+
+    public void setX(String x) {
+        this.x = x;
+    }
+
+    public String getY() {
+        return y;
+    }
+
+    public void setY(String y) {
+        this.y = y;
+    }
+
+    public Modelo() {
+    }
+    
+}
